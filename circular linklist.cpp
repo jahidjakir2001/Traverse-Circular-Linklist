1cb695f4c74e617f5051ad2e9954754bd821b60a
Md. Jahidul Islam <147086909+jahidjakir2001@users.noreply.github.com>
2024-07-01 12:50:58 +0600

    Add files via upload

---
+++ circular linklist.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+       int data;
+       struct node *next;
+};
+void Traverse(struct node *head)
+{
+      struct node *p=NULL;
+      p=head;
+      do
+      {
+            cout<<"Element:"<<p->data<<endl;
+            p=p->next;
+      }while(p->next!=head);
+      cout<<"Element:"<<p->data<<endl;
+}
+int main()
+{
+      struct node *head=NULL,*first=NULL,*second=NULL,*third=NULL;
+      head=(struct node *)malloc(sizeof(struct node));
+      first=(struct node *)malloc(sizeof(struct node));
+      second=(struct node *)malloc(sizeof(struct node));
+      third=(struct node *)malloc(sizeof(struct node));
+      head->data=10;
+      head->next=first;
+      first->data=20;
+      first->next=second;
+      second->data=30;
+      second->next=third;
+      third->data=40;
+      third->next=head;
+      cout<<"Traverse circular linklist:"<<endl;
+      Traverse(head);
+}
